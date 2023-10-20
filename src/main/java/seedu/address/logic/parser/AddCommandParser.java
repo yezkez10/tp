afedb0b571fe1b04c262e12ecf0f7269ff6829c5
yezkez <e1032428@u.nus.edu>
2023-10-20 10:40:48 +0800

    Fix error with multiple tags

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -43,8 +43,8 @@ public class AddCommandParser implements Parser<AddCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_GENDER, PREFIX_AGE,
                     PREFIX_ETHNIC, PREFIX_NRIC, PREFIX_ADDRESS, PREFIX_TAG);
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_NRIC,
-                PREFIX_GENDER, PREFIX_AGE, PREFIX_ETHNIC, PREFIX_ADDRESS);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_GENDER, PREFIX_AGE,
+                PREFIX_ETHNIC, PREFIX_NRIC, PREFIX_ADDRESS);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL,
                 PREFIX_GENDER, PREFIX_AGE, PREFIX_ETHNIC, PREFIX_NRIC)
