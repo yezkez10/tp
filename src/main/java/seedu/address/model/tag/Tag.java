4925064f471b02236d6f432aa1274fb1eed45074
yezkez <e1032428@u.nus.edu>
2023-10-21 15:45:40 +0800

    Fix tags

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -9,7 +9,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should not be empty";
     public static final String VALIDATION_REGEX = ".+";
 
     public final String tagName;
