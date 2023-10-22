2a88e917ceed09fa68aebed90a12d56eabb1b416
tohpinren <tohpinren@gmail.com>
2023-10-22 22:31:32 +0800

    Refactor: Rename to AddAppointmentCommand

--- src/main/java/seedu/address/logic/parser/ClinicAssistantParser.java
+++ src/main/java/seedu/address/logic/parser/ClinicAssistantParser.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AppointmentAddCommand;
+import seedu.address.logic.commands.AddAppointmentCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteAppointmentCommand;
@@ -85,7 +85,7 @@ public class ClinicAssistantParser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
-        case AppointmentAddCommand.COMMAND_WORD:
+        case AddAppointmentCommand.COMMAND_WORD:
             return new AddAppointmentParser().parse(arguments);
 
         case DeleteAppointmentCommand.COMMAND_WORD:
