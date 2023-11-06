b1b829528a737644e30a601e5a1a0381def604de
wj331 <e0389091@u.nus.edu>
2023-11-06 22:50:25 +0800

    Add more tests!

--- src/test/java/seedu/address/model/timeslots/UniqueTimeslotListTest.java
+++ src/test/java/seedu/address/model/timeslots/UniqueTimeslotListTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.timeslots;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
@@ -12,6 +13,28 @@ import seedu.address.model.timeslots.exceptions.DuplicateTimeslotException;
 import seedu.address.testutil.TypicalTimeslots;
 
 public class UniqueTimeslotListTest {
+    @Test
+    public void equal() {
+        UniqueTimeslotList uniqueTimeslotList = new UniqueTimeslotList();
+        //Heuristics: Equivalence partition: validUniqueTimeslotList/null/int/String
+        //same object
+        assertTrue(uniqueTimeslotList.equals(uniqueTimeslotList));
+
+        //different types
+        assertFalse(uniqueTimeslotList.equals(5));
+        assertFalse(uniqueTimeslotList.equals(""));
+        assertFalse(uniqueTimeslotList.equals(null));
+
+        UniqueTimeslotList uniqueTimeslotListCopy = new UniqueTimeslotList();
+        uniqueTimeslotList.add(TypicalTimeslots.DEFAULT_TIMESLOT);
+        uniqueTimeslotList.add(TypicalTimeslots.TIMESLOT_ONE);
+        uniqueTimeslotList.add(TypicalTimeslots.TIMESLOT_TWO);
+        uniqueTimeslotListCopy.add(TypicalTimeslots.DEFAULT_TIMESLOT);
+        uniqueTimeslotListCopy.add(TypicalTimeslots.TIMESLOT_ONE);
+        uniqueTimeslotListCopy.add(TypicalTimeslots.TIMESLOT_TWO);
+
+        assertTrue(uniqueTimeslotList.equals(uniqueTimeslotListCopy));
+    }
     @Test
     public void setTimeslotsWithNullTimeslot_failure() {
         UniqueTimeslotList uniqueTimeslotList = new UniqueTimeslotList();
