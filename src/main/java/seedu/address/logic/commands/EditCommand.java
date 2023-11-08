a998ce90ee0574ba31cc661c3bef990c535ab8af
yezkez <e1032428@u.nus.edu>
2023-11-08 16:30:19 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -249,11 +249,11 @@ public class EditCommand extends Command {
             this.tags = (tags != null) ? new HashSet<>(tags) : null;
         }
 
-            /**
-             * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
-             * if modification is attempted.
-             * Returns {@code Optional#empty()} if {@code tags} is null.
-             */
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
         public Optional<Set<Tag>> getTags() {
             return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
         }
